450c6ba4bf00b87a08ec424bb98ccea7cfc9cb49
Mark Schaefer <mark@qwizzle.de>
2018-12-13 21:37:03 +0100

    First version

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
 # git-branch-as-version
+
+A Maven plugin for setting the project version according to the current git branch.
+
+It is intended to be used on CI servers to set the version automatically before building and deploying, especially when using Gitflow.
+
+See also (todo: link other plugin)
+
+## Why has the Version to be Changed on Different Branches? 
+
+Let's say the current version on your develop branch is 2.1.4-SNAPSHOT.
+Let's further say you created some branches, e.g. feature/TTR-43-Perform-Foo or bugfix/TTR-123-Bar-Not-Printed.
+If branches are built automatically and also deployed to a Maven repository, not changing the version in the branches would result in functionally different artifacts with the same version. Obviously, this is bad for testing and keeping track of things in general.
+
+You may change the version manually after creating a new branch, which is error prone or might be forgotten. Additionally, this nearly always leads to merge conflicts which prevent automatic merging in Bitbucket or Gitlab.
+
+
+## What does this Plugin do for You?
+
+This plugin determines the current branch and sets the maven version accordingly.
+The version which is checked in is always in the form 1.2.3-SNAPSHOT. 
+If the plugin is activated automatically on your CI server you never have to worry again about the problems mentioned above.
+The correct version is set locally before the build proper and not commited to git.
+
+ 
+## Conventions
+
+The following default behaviour works well with Gitflow and issue-named branches, i.e. branch names which start with a issue number like TR-123.
+
+- The branch master will result in a release version number, e.g. 2.1.4
+- The branch develop will result in a snaphot version number, e.g. 2.1.4-SNAPSHOT
+- A branch like release/TR-432 will result in a snaphot release candidate version, e.g. 2.1.4-TR-432-RC-SNAPSHOT
+- A branch like xyz/TR-123-foo-foo will result in a snapshot version containing the issue number, e.g. 2.1.4-TR-123-SNAPSHOT.
+It is not differentiated between feature, hotfix, bugfix, release or other branch types except release branches.
+
+
+## Usage
+
+This plugin executes internally the version plugin (todo: link) which sets the new version (also for module projects).
+Depending on the Maven version and other circumstances this might or might not work with one maven execution. To be sure, it is best to excute maven twice:
+
+````bash
+$ mvn clean
+$ mvn deploy
+````
+
+instead of
+
+````bash
+$ mvn clean deploy
+````
+
+with the following configuration:
+
+
+````xml
+<plugin>
+	<groupId>com.github.mark--</groupId>
+	<artifactId>git-branch-as-version</artifactId>
+	<version>0.0.1-SNAPSHOT</version>
+	<executions>
+		<execution>
+			<id>set git branch version</id>
+			<phase>pre-clean</phase>
+			<goals>
+				<goal>set</goal>
+			</goals>
+			<inherited>true</inherited>
+			<configuration>
+				<branchPattern>...</branchPattern>
+				<versionPattern>...</versionPattern>
+				<branchFromEnvironment>...</branchFromEnvironment>
+				<branchEnvironmentVariable>...</branchEnvironmentVariable>
+			</configuration>
+		</execution>
+	</executions>
+</plugin>
+````
+
+
+## Configuration
+
+The plugin supports the follwing configuration options. The default works with JIRA issue IDs in the branch name and Jenkins as build server.
+
+
+|Property|Default|Description|
+|--------|-------|-----------|
+|branchPattern|(\\w+\\-\\w+).*|A Java regex which is matched against the last component of the git branch name. The first capturing group is taken as the branch identifier which is used for the final Maven version.|
+|versionPattern|(\\d+\\.\\d+\\.\\d+).*|A Java regex which is matched against the project version. The first capturing group is taken as the version for the final Maven version.
+|branchFromEnvironment|true|Detect the current branch by some environment variable. If set to false, the plugin tries to detect the version by looking into the git repository.|
+|branchEnvironmentVariable|GIT_BRANCH|The environment variable which contains the current branch. If the environment variable is not present, the plugin tries to detect the version by looking into the git repository.|
+
+
---
+++ src/main/java/de/qwizzle/tool/branchinversion/SetMavenVersionFromGitBranch.java
@@ -0,0 +1,264 @@
+package de.qwizzle.tool.branchinversion;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.artifactId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.name;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.version;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.BuildPluginManager;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+/**
+ * Mojo which Maven Mojo, das den aktuellen git Branch gegen die Versionsnummer abgleicht und den
+ * Maven Build failen lässt, falls das Issue aus dem Branch nicht in der Version vorkommt. Die
+ * branches develop, master und alle release Branches werden ignoriert.
+ *
+ * @author Mark Schäfer
+ */
+@Mojo(name = "set", defaultPhase = LifecyclePhase.PRE_CLEAN, threadSafe = false, requiresProject = false,
+    inheritByDefault = false)
+public class SetMavenVersionFromGitBranch extends AbstractMojo
+{
+    private static final int MAX_LEVENSHTEIN_EDIT_DISTANCE = 4;
+
+    static final String ISSUE_KEY_REGEX = "\\w\\w+\\-\\d+";
+
+    @Parameter(defaultValue = "${project}", readonly = true)
+    private MavenProject project;
+
+    @Parameter(defaultValue = "${session}", readonly = true)
+    private MavenSession mavenSession;
+
+    @Component
+    private BuildPluginManager pluginManager;
+
+    @Parameter(name = "branchPattern", defaultValue = "(\\w+\\-\\w+).*")
+    private String branchPattern;
+
+    @Parameter(name = "versionPattern", defaultValue = "(\\d+\\.\\d+\\.\\d+).*")
+    private String versionPattern;
+
+    @Parameter(name = "branchFromEnvironment", defaultValue = "true")
+    private boolean branchFromEnvironment;
+
+    @Parameter(name = "branchEnvironmentVariable", defaultValue = "GIT_BRANCH")
+    private String branchEnvironmentVariable;
+
+    @Override
+    public void execute() throws MojoExecutionException
+    {
+        String version = getVersion();
+        String branch = getBranch();
+
+        if (branch.equals("develop"))
+        {
+            setVersion(String.format("%s-SNAPSHOT", version));
+            return;
+        }
+
+        if (branch.equals("master"))
+        {
+            setVersion(version);
+            return;
+        }
+
+        Matcher branchMatcher = Pattern.compile(branchPattern).matcher(branch);
+
+        if (!branchMatcher.matches())
+        {
+            throw new MojoExecutionException("Could not parse branch name: " + branch);
+        }
+
+        if (branch.contains("release"))
+        {
+            String issue = branchMatcher.group(1);
+            setVersion(String.format("%s-%s-RC-SNAPSHOT", version, issue));
+            return;
+        }
+        else
+        {
+            String issue = branchMatcher.group(1);
+            setVersion(String.format("%s-%s-SNAPSHOT", version, issue));
+            return;
+        }
+
+    }
+
+    /**
+     * @return the first capturing group matched by the regex {@link #versionPattern}, as default
+     *         the MAJOR.MINOR.MICRO part of the version, where all three parts are expected to be
+     *         numbers
+     * @throws MojoExecutionException if the version does not match or no capturing group was
+     *         defined
+     */
+    String getVersion() throws MojoExecutionException
+    {
+        Matcher versionMatcher = Pattern.compile(versionPattern).matcher(project.getVersion());
+        if (!versionMatcher.matches())
+        {
+            throw new MojoExecutionException("Could not parse Maven version: " + project.getVersion());
+        }
+
+        try
+        {
+            return versionMatcher.group(1);
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            throw new MojoExecutionException(
+                "Could not parse Maven version due to missing capturing group in regex: " + versionPattern,
+                e);
+        }
+    }
+
+    void setVersion(String newVersion) throws MojoExecutionException
+    {
+        getLog().info("Setting version: " + newVersion);
+        executeMojo(
+            plugin(
+                groupId("org.codehaus.mojo"),
+                artifactId("versions-maven-plugin"),
+                version("2.3")),
+            goal("set"),
+            configuration(
+                element(name("generateBackupPoms"), "false"),
+                element(name("newVersion"), newVersion)),
+            executionEnvironment(
+                project,
+                mavenSession,
+                pluginManager));
+    }
+
+    String getBranch() throws MojoExecutionException
+    {
+        if (branchFromEnvironment)
+        {
+            getLog().info("Taking branch name from environment variable: " + branchEnvironmentVariable);
+            String branch = System.getenv(branchEnvironmentVariable);
+            if (branch == null)
+            {
+                getLog().warn("Environment variable not defined: " + branchEnvironmentVariable);
+                List<String> possibleVariables = getPossibleVariables(branchEnvironmentVariable);
+                if (!possibleVariables.isEmpty())
+                {
+                    getLog()
+                        .warn("Did you mean one of: "
+                            + possibleVariables.stream().sorted().collect(joining(", ")));
+                }
+
+            }
+            else
+            {
+                return lastPartOfBranch(branch);
+            }
+        }
+
+        String branch = lastPartOfBranch(getBranchOrRaiseException());
+
+        getLog().info("Found branch: " + branch);
+        return branch;
+    }
+
+    List<String> getPossibleVariables(String gitBranchEnvironmentVariable2)
+    {
+        List<String> result = new LinkedList<>();
+        for (String env : System.getenv().values())
+        {
+            if (LevenshteinDistance.getDefaultInstance().apply(env,
+                gitBranchEnvironmentVariable2) <= MAX_LEVENSHTEIN_EDIT_DISTANCE)
+            {
+                result.add(env);
+            }
+        }
+
+        return result;
+    }
+
+    String extractIssue(Pattern pattern, String branchName)
+    {
+        Matcher matcher = pattern.matcher(branchName);
+        if (matcher.find())
+        {
+            String issue = matcher.group(0);
+            return issue;
+        }
+        return null;
+
+    }
+
+    String getBranchOrRaiseException() throws MojoExecutionException
+    {
+        try
+        {
+            Repository repo = new FileRepositoryBuilder().setGitDir(new File(".git")).build();
+            String branch = repo.getBranch();
+            if (!isObjectId(branch))
+            {
+                return branch;
+            }
+
+            List<String> branchesForCommit = branchesForCommit(repo, repo.resolve(branch));
+
+            if (branchesForCommit.size() != 1)
+            {
+                throw new MojoExecutionException("Ambigious branches detected: " + branchesForCommit
+                    + ". Consider using the 'branchFromEnvironment' and 'branchEnvironmentVariable' options.");
+            }
+
+            return branchesForCommit.get(0);
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException(
+                "Could not get git branch from repository. Maybe no git repository present?", e);
+        }
+    }
+
+    boolean isObjectId(String branch)
+    {
+        return branch.toLowerCase().matches("[0123456789abcdef]+");
+    }
+
+    List<String> branchesForCommit(Repository repo, ObjectId objectId) throws MojoExecutionException
+    {
+        return repo.getAllRefs().values().stream()
+            .filter(ref -> ref.getObjectId().equals(objectId))
+            .map(ref -> ref.getName())
+            .map(this::lastPartOfBranch)
+            .filter(ref -> !"HEAD".equals(ref))
+            .collect(toList());
+    }
+
+    String lastPartOfBranch(String branch)
+    {
+        String[] split = branch.split("/");
+        return split[split.length - 1];
+    }
+
+}
